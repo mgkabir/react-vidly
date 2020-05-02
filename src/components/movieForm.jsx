c23d261c4e64e097b0dc89c29635d5e03c46f1d6
Mohammad Kabir <mgkabir@gmail.com>
2020-05-02 15:01:50 +1000

    implemented router

---
+++ src/components/movieForm.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+class MovieForm extends Component {
+  state = {};
+  render() {
+    return <h4>Movie Form</h4>;
+  }
+}
+
+export default MovieForm;
---
+++ src/components/notFound.jsx
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+const NotFound = () => {
+  return <h4>Not Found</h4>;
+};
+
+export default NotFound;

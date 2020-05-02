01ff9d8d592b7391c715525e44d829e845558c2e
Mohammad Kabir <mgkabir@gmail.com>
2020-05-02 22:46:58 +1000

    LoginForm created, linked to navBar link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import NavBar from "./components/navBar";
+import LoginForm from "./components/loginForm";
 import "./App.css";
 
 class App extends Component {
@@ -14,6 +15,7 @@ class App extends Component {
         <NavBar />
         <main className="container">
           <Switch>
+            <Route path="/login" component={LoginForm}></Route>
             <Route path="/movies" component={Movies}></Route>
             <Route path="/customers" component={Customers}></Route>
             <Route path="/rentals" component={Rentals}></Route>
---
+++ src/components/loginForm.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class LoginForm extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h4>Login Form</h4>
+        <form>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input id="username" type="text" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input id="password" type="text" className="form-control" />
+          </div>
+          <button className="btn btn-primary">Login</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;

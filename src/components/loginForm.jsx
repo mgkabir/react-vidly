6ad377c588125ff87e086f7afab3635de65de726
Mohammad Kabir <mgkabir@gmail.com>
2020-05-02 23:06:36 +1000

    Default form submission behaviour prevented

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
-  state = {};
+  handleSubmit = (e) => {
+    e.preventDefault();
+    // call server and save value here
+    console.log("Form Submitted .. ");
+  };
+
   render() {
     return (
       <div>
         <h4>Login Form</h4>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
             <input id="username" type="text" className="form-control" />
